278f600716ccff3d79f66cbfe43850377a233cf5
Jose Vargas <jg.vargas@pappcorn.com>
2018-01-22 20:07:58 -0500

    cambios gitignore

--- dist/router/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var index = express_1.Router();
-index.get('/', function (req, res, nex) {
-    res.send("Hola index 8");
-});
-exports.default = index;
-//# sourceMappingURL=index.js.map
